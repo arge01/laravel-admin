0fd2fe85c58b493bc0959487e83c6c1f22c2cdfb
arge01 <gevenci.arif@gmail.com>
2020-01-30 02:45:49 +0200

    route get

--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -5,7 +5,7 @@
     <main class="d-flex justify-content-center align-items-center min-vh-100">
         <p>
             <span style="position: relative; top: 3px; font-size: 2.1em">{</span> 
-            <a href="{{route('anasayfa')}}"><i class="fas fa-layer-group"></i></a> Developer By 
+            <a href="{{route('sayfa', 'iletisim')}}"><i class="fas fa-layer-group"></i></a> Developer By 
             <span class="font-weight-bold text-light bg-dark p-1">Arif GEVENCI</span>
             <span style="position: relative; top: 3px; font-size: 2.1em">}</span>
         </p>

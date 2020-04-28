0385ee6fe0abe9fc138baeaeee4834894701dd53
Arif Gevenci <agevenci@islem.com.tr>
2020-04-28 18:42:05 +0300

    commit
    commit

--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -13,15 +13,6 @@ class AdminTableSeeder extends Seeder
     public function run()
     {
         DB::table('uyeler')->insert([
-            'adsoyad'             => 'Arif GEVENCİ',
-            'email'               => 'gevenci.arif@gmail.com',
-            'sifre'               => '$2y$10$JqOZ05Gme4QlGCgbk.gZQuAmkaKf5jksyVlOR8kj49SVq5neY8HsG',
-            'aktivasyon_anahtari' => '1',
-            'remember_token'      => '1',
-            'rutbeler'            => 1
-        ]);
-
-        DB::table('uyeler')->insert([
             'adsoyad'             => 'Yeni Admin',
             'email'               => 'info@domain-adi.com.tr',
             'sifre'               => Hash::make(123),
